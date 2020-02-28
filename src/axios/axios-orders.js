e25ad84a8dd48b944b253aab404b3850b5ef1eb9
chripas88 <chri-pas@hotmail.com>
2020-02-28 13:31:27 +0000

    no-auth-db

--- src/axios/axios-orders.js
+++ src/axios/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'https://the-burger-builder-ce48e.firebaseio.com/'
+	baseURL: 'https://the-burger-builder-no-auth.firebaseio.com/'
 });
 
 export default instance;
